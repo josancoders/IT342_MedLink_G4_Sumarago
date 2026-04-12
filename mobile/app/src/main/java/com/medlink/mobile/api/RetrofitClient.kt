040dd978c9f8ac2723e6cb2a95497d47f75036c3
josan <josansumarago00@gmail.com>
2026-04-12 22:39:29 +0800

    small update

--- mobile/app/src/main/java/com/medlink/mobile/api/RetrofitClient.kt
+++ mobile/app/src/main/java/com/medlink/mobile/api/RetrofitClient.kt
@@ -7,7 +7,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.100:8080/"
+    // Use 10.0.2.2 for Android emulator to reach host machine
+    // Use localhost:8080 for physical device on same network
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     private val okHttpClient = OkHttpClient.Builder()
         .connectTimeout(10, TimeUnit.SECONDS)
